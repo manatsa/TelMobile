159c53bbecf0e3841834f3635c47c04b38c06f81
Manatsa Chinyeruse <manatsachinyeruse@gmail.com>
2017-10-29 16:45:46 +0200

    Have Added Tests for Tabs which are now successful

--- www/js/forgotPassword.js
+++ www/js/forgotPassword.js
@@ -1,17 +1,29 @@
+
+function sendMail(user)
+{
+    var link = "mailto:manatsachinyeruse@gmail.com"+
+        "?cc="+
+        "&subject=" + escape("Password Recovery For MSISDN :" + user.msisdn)+
+        "&body=" + escape("Good day.\n\nTelecel Mobile App Password is :" + user.pin);
+    window.location.href = link;
+    $.mobile.loading("hide")
+}
+
 function sendPasswordViaEmail() {
 $.mobile.loading("show")
 
     NativeStorage.getItem("user", function (user) {
-
+        //sendMail(user)
         Email.send("manatsachinyeruse@gmail.com",
             "manatsachinyeruse@gmail.com",
             "Password Recovery For MSISDN :" + user.msisdn,
             "Good day.\n\nTelecel Mobile App Password is :" + user.pin,
-            "smtp.google.com",
-            "manatsachinyeruse@gmail.com",
-            "Manat5achin5");
-        $.mobile.loading("hide")
-        navigator.notification.alert("Email Sent Successfully!", function () {}, "Pin Recovery", "OK")
+            {token:f9d1b063-e5e5-43f6-89d8-ec02c0ab4268}
+            , function () {
+                $.mobile.loading("hide")
+                navigator.notification.alert("Email Sent Successfully!", function () {}, "Pin Recovery", "OK")
+            });
+
     });
 
 }
